e416de750a620ea80fe9ac7eb74b273077b0fd54
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-12 17:35:30 -0300

    adicionadas configurações do contexto

--- Infraestrutura/Dbs/DbContexto.cs
+++ Infraestrutura/Dbs/DbContexto.cs
@@ -1,8 +1,27 @@
 using Microsoft.EntityFrameworkCore;
+using MinimalAPIs.Dominio.Entidades;
 
 namespace MinimalAPIs.Infraestrutura.Dbs;
 
 public class DbContexto : DbContext
 {
+    private readonly IConfiguration _configuracaoAppSettings;
 
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured) {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("sqlserver")?.ToString();
+            if(!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseSqlServer(stringConexao);
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using MinimalAPIs.Infraestrutura.Dbs;
+using MinimalAPIs.Dominio.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlserver"));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalAPIs.Dominio.DTOs.LoginDTO loginDTO) => {
+app.MapPost("/login", (LoginDTO loginDTO) => {
     if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     {
         return Results.Ok("Login com sucesso");

6e5aba471627e03968c09f92d435457266b5cdac
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-12 16:42:23 -0300

    LoginDTO movido para arquivo

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,7 @@
+namespace MinimalApis.DTOs;
+    
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO loginDTO) => {
+app.MapPost("/login", (MinimalApis.DTOs.LoginDTO loginDTO) => {
     if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     {
         return Results.Ok("Login com sucesso");
@@ -15,10 +15,3 @@ app.MapPost("/login", (LoginDTO loginDTO) => {
 });
 
 app.Run();
-
-
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Senha { get; set; } = default!;
-}

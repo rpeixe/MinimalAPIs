7ca65e6cbe76dcd6bd98242f3cbdbbc9a5de3565
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-12 16:38:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
+
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}

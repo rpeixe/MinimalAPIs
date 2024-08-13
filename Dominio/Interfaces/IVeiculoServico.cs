6035cbfc1fc45b4e4f52385e2fb5e1d770b1fe80
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-13 10:41:00 -0300

    Consertado valor padrão

--- Dominio/Interfaces/IVeiculoServico.cs
+++ Dominio/Interfaces/IVeiculoServico.cs
@@ -5,7 +5,7 @@ namespace MinimalAPIs.Dominio.Interfaces;
 
 public interface IVeiculoServico
 {
-    List<Veiculo> Todos(int? pagina = 1, string? nome = null, string? marca = null);
+    List<Veiculo> Todos(int? pagina, string? nome = null, string? marca = null);
     Veiculo? BuscaPorId(int id);
     void Incluir(Veiculo veiculo);
     void Atualizar(Veiculo veiculo);

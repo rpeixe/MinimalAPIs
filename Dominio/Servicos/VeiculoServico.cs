a62f1b0a836251115ca2f264470f35562e6065be
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-13 08:43:41 -0300

    Serviço de veiculos modificado para o argumento página ser opcional

--- Dominio/Servicos/VeiculoServico.cs
+++ Dominio/Servicos/VeiculoServico.cs
@@ -37,7 +37,7 @@ public class VeiculoServico : IVeiculoServico
         _contexto.SaveChanges();
     }
 
-    public List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null)
+    public List<Veiculo> Todos(int? pagina, string? nome = null, string? marca = null)
     {
         var query = _contexto.Veiculos.AsQueryable();
 
@@ -52,7 +52,10 @@ public class VeiculoServico : IVeiculoServico
 
         int itensPorPagina = 10;
 
-        query = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+        if(pagina != null)
+        {
+            query = query.Skip(((int)pagina - 1) * itensPorPagina).Take(itensPorPagina);
+        }
 
         return query.ToList();
     }

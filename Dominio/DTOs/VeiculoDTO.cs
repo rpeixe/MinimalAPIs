547e688e21f2326370f45a516c6004d2c72ac442
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-13 08:38:05 -0300

    Adicionado DTO para veículos

---
+++ Dominio/DTOs/VeiculoDTO.cs
@@ -0,0 +1,8 @@
+namespace MinimalAPIs.Dominio.DTOs;
+    
+public record VeiculoDTO
+{
+    public string Nome { get; set; } = default!;
+    public string Marca { get; set; } = default!;
+    public int Ano { get; set; } = default!;
+}

24c0e743d5e64dfa6c13c685fb2a5d5f22d91deb
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-13 10:46:36 -0300

    Seção de perfil tornada nullable

--- Dominio/DTOs/AdministradorDTO.cs
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -6,5 +6,5 @@ public class AdministradorDTO
 {
     public string Email { get; set; } = default!;
     public string Senha { get; set; } = default!;
-    public Perfil Perfil { get; set; } = default!;
+    public Perfil? Perfil { get; set; } = default!;
 }

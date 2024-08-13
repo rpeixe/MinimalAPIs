d191eb271161f9927d91f70a57f0972c61e18e22
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-12 21:34:04 -0300

    Senha e perfil agora são obrigatórios

--- Dominio/Entidades/Administrador.cs
+++ Dominio/Entidades/Administrador.cs
@@ -13,9 +13,11 @@ public class Administrador
     [StringLength(255)]
     public string Email { get; set; } = default!;
 
+    [Required]
     [StringLength(50)]
     public string Senha { get; set; } = default!;
 
+    [Required]
     [StringLength(10)]
     public string Perfil { get; set; } = default!;
 }

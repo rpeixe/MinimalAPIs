1cbcd83ac5286672a7d9b1c8961c396035326f3a
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2024-08-13 10:41:40 -0300

    Criado DTO para administradores

---
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -0,0 +1,10 @@
+using MinimalAPIs.Dominio.Enums;
+
+namespace MinimalAPIs.Dominio.DTOs;
+    
+public class AdministradorDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+    public Perfil Perfil { get; set; } = default!;
+}
